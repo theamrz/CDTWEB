16df64f2df23d435859a13b4b8a3a2031e126053
AmirHoseinRezapour <127383183+theamrz@users.noreply.github.com>
2025-09-22 01:36:58 -0300

    Initial Commit

---
+++ CDTWEB/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CDTWEB
+//
+//  Created by AmirHosein Rezapour on 6/31/1404 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
